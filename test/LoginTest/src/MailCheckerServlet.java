445e04bff91853a7a8a2250d665e1353dc94c501
smtarasov <sm.tarasov@gmail.com>
2014-12-03 09:28:48 +0200

    messy commit

---
+++ test/LoginTest/src/MailCheckerServlet.java
@@ -0,0 +1,93 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+@WebServlet(name = "MailCheckerServlet", urlPatterns = "/MailCheckerServlet")
+public class MailCheckerServlet extends HttpServlet {
+
+    protected static boolean checked;
+
+    protected void doPost(javax.servlet.http.HttpServletRequest request,
+                          javax.servlet.http.HttpServletResponse response)
+            throws javax.servlet.ServletException, IOException {
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            System.out.println("Driver loading success!");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("====>>> ClassNotFoundException");
+        }
+
+
+        String CookieString = null;
+
+        // Ищем нужный куки
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("XString")) {
+                CookieString = cookie.getValue();
+            }
+        }
+        // Получаем от клиента строку и хэшируем её.
+        String ClientString = request.getParameter("ClientStr");
+        System.out.println(ClientString);
+        String HASHClientString = "";
+        try {
+            HASHClientString = UtilHash.getHash(ClientString);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        String Result;
+        if (CookieString != null) {         // FALSE == Нужный куки не найден.
+            if (HASHClientString.equals(CookieString)) {
+                Result = "Адрес " + RegServlet.NewUserEmail + " подтверждён!<br>";
+                checked = true;
+
+
+                String dbusername = "postgres";
+                String dbpwd = "123";
+                String dburl = "jdbc:postgresql://localhost:5432/poll_2";
+
+                try {
+                    System.out.println("====>>> Start Connection");
+                    Connection db = DriverManager.getConnection(dburl, dbusername, dbpwd);
+                    System.out.println("Connection success!");
+                    Statement st = db.createStatement();
+                    String sql = "INSERT INTO group_entries " +
+                            "(user_id, group_id, entry_author) VALUES " +
+                            "('" + RegServlet.user_id + "', '3', '" + RegServlet.user_id + "')";
+                    st.executeUpdate(sql);
+
+
+                    st.close();
+                    db.close();
+
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+
+            } else {
+                Result = "Подтверждение адреса e-mail провалилось! <br>\n" +
+                        "— Вернитесь на начальную страницу<br>\n" +
+                        "— Повторите отправку проверочного письма.<br>";
+                checked = false;
+            }
+
+        } else {
+            Result = "Cookie Устарели либо не были установлены.<br>\n";
+        }
+        request.setAttribute("Message", Result);
+        getServletContext().getRequestDispatcher("/Report.jsp").forward(
+                request, response);
+    }
+}
---
+++ test/LoginTest/web/index.jsp
@@ -0,0 +1,33 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Главная</title>
+</head>
+<body>
+<form action="AuthServlet" method="post">
+    <table border="0" width="35%" align="center">
+        <caption><h2>Главная страница</h2></caption>
+        <tr>
+            <td width="30%">Логин/e-mail</td>
+            <td><input type="text" name="UserName" size="25" title="UserLogin"/></td>
+        </tr>
+        <tr>
+            <td>Пароль</td>
+            <td><input type="text" name="UserPWD" size="25" title="UserPWD"/></td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center"><input type="submit" value="Вход"/></td>
+        </tr>
+    </table>
+</form>
+<br/>
+<br/>
+
+<form action="Register.jsp" method="post">
+    <div style="text-align: center;">
+        <button type="submit">Регистрация</button>
+    </div>
+</form>
+</body>
+</html>
